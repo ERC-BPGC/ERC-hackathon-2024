b7166f46086dc40077a3625914b2f23e7d2006b1
Ritwik <ritwiksha2027@gmail.com>
2024-06-25 21:20:14 +0530

    Added coordinate transformation

--- ros_world/src/path_planner.py
+++ ros_world/src/path_planner.py
@@ -1,6 +1,10 @@
 ########################################################################################################
 
 # Wassup! This is where your Path Planning code goes for the Automation Task of the ERC Hackathon '24
+#
+# Remember to use the transform points function in obstacle_detection.py to get points corresponding to
+#         your path in the gazebo coordinate system. This is what controller.py should use!!
+#
 #                                       Happy Planning!!!
 
 ########################################################################################################
