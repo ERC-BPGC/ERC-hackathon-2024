39f883a79d695f0204502634a398a6934b24d80b
Ritwik <ritwiksha2027@gmail.com>
2024-07-15 16:51:18 +0530

    Modified roads.png and updated cone locations

--- hackathon_automation/src/path_planner.py
+++ hackathon_automation/src/path_planner.py
@@ -13,21 +13,21 @@
 
 # Cooridinates of the cones as per the map (image version) - look at get_landmarks.py & obstacle_detection.py
 # ERC Room: 109, 296
-# Cone 1: 155, 205
-# Cone 2: 299, 142
-# Cone 3: 506, 536
-# Cone 4: 239, 643
-# Cone 5: 193, 329
+# Cone 1: 159, 208
+# Cone 2: 296, 142
+# Cone 3: 502, 539
+# Cone 4: 244, 640
+# Cone 5: 190, 326
 
 # You can look at the visualization function in demo.py to see how to visualize the path
 
 # These are the coordinates you have to go to in Gazebo
 # Start Location (ERC Room): 12.994061, 14.233000
-# Cone 1 (near Sandbox): 19.964537, 10.696421
-# Cone 2 (NAB): 24.802801, -0.369133
-# Cone 3 (VGH): -5.516354, -16.286877
-# Cone 4 (SAC): -13.724400, 4.257628
-# Cone 5 (BDome): 10.395211, 7.784442
+# Cone 1 (near Sandbox): 19.765875, 10.429128
+# Cone 2 (NAB): 24.820259, -0.089209
+# Cone 3 (VGH): -5.729289, -15.973051
+# Cone 4 (SAC): -13.520048, 3.922464
+# Cone 5 (BDome): 10.654515,8.028189
 # Cones colors will be randomized
 
 # There is a function to convert the obstacle_detection-> Maze-> points point_transform
